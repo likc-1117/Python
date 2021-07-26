ec36e1626b4aa81f14d95c78a445853bfeb52919
李可灿 <69657384+likc-1117@users.noreply.github.com>
2021-07-26 11:39:24 +0800

    Add files via upload

---
+++ Interface_Test_Frame/Result_Handle/Actual_Result.py
@@ -0,0 +1,8 @@
+#coding = utf-8
+
+import openpyxl
+class actual_result(object):
+    
+    
+    def __init__(self):
+        pass
---
+++ Interface_Test_Frame/Result_Handle/Result_Data_Handle.py
@@ -0,0 +1,15 @@
+#coding = utf-8
+
+
+
+
+class result_data_handle(object):
+    
+
+        
+        
+        
+    def result_data_check(self,result_data,expect_result):
+        if result_data == expect_result:
+            return True
+        return False
