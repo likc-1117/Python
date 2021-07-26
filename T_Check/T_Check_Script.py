46da67c515bc8dfb25381dceabb9c345e9f6a36d
李可灿 <69657384+likc-1117@users.noreply.github.com>
2021-07-26 11:41:54 +0800

    Add files via upload

---
+++ T_Check/T_Check_Script.py
@@ -0,0 +1,22 @@
+#coding=utf-8
+
+from T_Check.T_Check_Base.T_Control_Check import *
+import serial
+import serial.tools.list_ports
+
+
+# t_control_check.operation_t_machine(0,15)
+# t_control_check.operation_t_machine(1,15)
+# t_control_check.t_script_run('',3)
+
+
+# device = serial.Serial()
+# device.baudrate = 115200
+# device.bytesize = 8
+# device.stopbits = 1
+# device.timeout = 60
+# port_list = list(serial.tools.list_ports.comports())
+# print(port_list)
+# for port_name in port_list:
+#     print(str(port_name.device)+':'+str(port_name.pid)+':'+str(port_name.apply_usb_info()))
+
